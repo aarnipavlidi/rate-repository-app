bbed5d06ca406a21dd4dd402c46d6bf4f92ffa00
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-08-01 11:14:49 +0300

    Exercise 10.12: environment variables

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,7 +1,12 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants'; // Otetaan käyttöön "Constants" komponentti => "expo-constants" kirjaston kautta sovelluksen käytettäväksi.
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.171:5000/graphql',
+  // Tehtävää varten "Exercise 10.12: environment variables" varten lisätty tiedostoon
+  // "app.config.js" uusi objektin arvo "extra", jonka sisältä löytyy "server" arvo,
+  // joka on yhtä kuin => "process.env.APOLLO_URI". Ja kyseiseen arvoon pääsemme
+  // käsiksi käyttämällä "Constants" funktiota ja => "manifest.extra.server".
+  uri: Constants.manifest.extra.server,
 });
 
 const createApolloClient = () => {
